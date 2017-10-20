79bf8defcaaacee062d74d7fdd733a0fb6017601
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-10-20 14:21:05 -0500

    crete payment_type model and add association

---
+++ migrations/20171020191243-create-payment-type.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Payment_Types', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      account_number: {
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Payment_Types');
+  }
+};
---
+++ models/payment_type.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Payment_Type = sequelize.define('Payment_Type', {
+    id: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    account_number: DataTypes.INTEGER,
+    user_id: DataTypes.INTEGER
+  }, {timestamps: false});
+  
+  Payment_Type.associate = (models) => {
+    Payment_Type.hasMany(models.Order, {
+      foreignKey: 'payment_type_id'
+    }); 
+  };
+  return Payment_Type;
+};
