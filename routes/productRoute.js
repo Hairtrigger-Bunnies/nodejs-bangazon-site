90cfc8cdc7083f5c0ff3a0b4916dbee5e5758eeb
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-10-23 11:18:17 -0500

    file making

---
+++ routes/productRoute.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const { Router } = require('express');
+const router = Router();
+
+const {
+    getProdDetail
+  } = require('../controllers/productCtrl.js');
+
+router.get('/product/:id', getProdDetail);
