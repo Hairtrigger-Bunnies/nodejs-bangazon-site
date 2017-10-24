e4dc0b6f7c7af79f35dc438b343b4b7501d80524
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-10-24 11:57:53 -0500

    display user's payment types and orders, by order id and date

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -8,15 +8,19 @@ module.exports.getOneUser = (req, res, next) => {
   })
   .then( (foundUser) => {
     let user = foundUser[0].dataValues;
-    // console.log("USER", user);
-    let bigUserPmt = foundUser[0].dataValues.Payment_Types;
-    let userPmt = bigUserPmt.map(function(each) {
+    console.log("USER", user);
+    // let bigUserPmt = foundUser[0].dataValues.Payment_Types;
+    let userPmt = foundUser[0].dataValues.Payment_Types.map(function(each) {
       return each.dataValues;
     });
-    console.log("users Payment types?", userPmt);
+    let userOrders = foundUser[0].dataValues.Orders.map(function(each) {
+      return each.dataValues;
+    });
+    console.log("User's orders?", userOrders);
     res.render('userAcctSettings', {
       user,
-      userPmt
+      userPmt,
+      userOrders
     })
   })
 };
