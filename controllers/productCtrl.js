227759de96cce95469da03f2303d5b8e825dba55
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-10-24 11:03:00 -0500

    work on verifying user login on page load

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -1,42 +1,42 @@
 'use strict'
 const passport = require('passport');
 
-
 module.exports.addProduct = (req, res, next) => {
-  const { Product } = req.app.get('models'); 
-  Product.create({
-    title:req.body.title,
-    description:req.body.description,
-    //JM: this uid is being pulled from the updated 'user' var via git passport when isloggedin. same as req.passport.sessions.user
-    user_id:req.user.id,
-    price:req.body.price,
-    quantity_avail:req.body.quantity_avail
-  })
-  .then( (data)=>{
-    res.status(200).redirect(`/product/${data.dataValues.id}`);
-    })
-    .catch( (err) => {
-      res.status(200).json(err);
-    })
+	const { Product } = req.app.get('models');
+	Product.create({
+			title: req.body.title,
+			description: req.body.description,
+			//JM: this uid is being pulled from the updated 'user' var via git passport when isloggedin. same as req.passport.sessions.user
+			user_id: req.user.id,
+			price: req.body.price,
+			quantity_avail: req.body.quantity_avail
+		})
+		.then((data) => {
+			res.status(200).redirect(`/product/${data.dataValues.id}`);
+		})
+		.catch((err) => {
+			res.status(200).json(err);
+		})
 };
 
 // EXPORTS MODULE THAT BRINGS UP FORM TO ADD A NEW PRODUCT TO SELL- HITS productRoute
-module.exports.displayAddProduct = (req, res, nect) => {
+module.exports.displayAddProduct = (req, res, next) => {
+  const { }
   res.render('new-product-form');
 };
 
 //get one product's details to PUG to the dom(-el)
 module.exports.getProdDetail = (req, res, next) => {
-    const { Product } = req.app.get('models');
-    let prodId = req.params.id;
-    Product.findById(prodId)
-    .then( (foundProd) => {
-        let singleProd = foundProd.dataValues;
-        res.render('singleProdDetail', { //pass in the info the PUG will need to render this info
-            singleProd
-        })
-    })
-    .catch( (err) => {
-        next(err)
-    })
+	const { Product } = req.app.get('models');
+	let prodId = req.params.id;
+	Product.findById(prodId)
+		.then((foundProd) => {
+			let singleProd = foundProd.dataValues;
+			res.render('singleProdDetail', { //pass in the info the PUG will need to render this info
+				singleProd
+			})
+		})
+		.catch((err) => {
+			next(err)
+		})
 };
