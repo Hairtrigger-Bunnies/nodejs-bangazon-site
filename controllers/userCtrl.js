eba6368ff576342fbdb242888d3e0a13b519724f
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-10-24 11:07:28 -0500

    find with all associations

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -2,5 +2,10 @@
 
 module.exports.getOneUser = (req, res, next) => {
   const { User } = req.app.get('models'); 
-  
-}
+  User.findAll({
+    include: [{
+      all: true
+    }],
+    where: {id: req.params.id }
+  })
+};
