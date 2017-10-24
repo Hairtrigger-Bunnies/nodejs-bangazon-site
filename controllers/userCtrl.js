9be49442e7232253251b6003db0e29fdf5ab9d2f
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-10-24 11:25:22 -0500

    set up feasible path for self as user 51

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -3,9 +3,14 @@
 module.exports.getOneUser = (req, res, next) => {
   const { User } = req.app.get('models'); 
   User.findAll({
-    include: [{
-      all: true
-    }],
+    include: [{ all: true }],
     where: {id: req.params.id }
+  })
+  .then( (foundUser) => {
+    let user = foundUser[0].dataValues;
+    console.log("USER", user);
+    res.render('userAcctSettings', {
+      user
+    })
   })
 };
