7331b13864c6b5a1858c2704c631c7b0c6611eee
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-10-24 11:50:31 -0500

    change the JSON file to have 'name' instead of 'payment_type' for the db to recognize it

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -8,9 +8,15 @@ module.exports.getOneUser = (req, res, next) => {
   })
   .then( (foundUser) => {
     let user = foundUser[0].dataValues;
-    console.log("USER", user);
+    // console.log("USER", user);
+    let bigUserPmt = foundUser[0].dataValues.Payment_Types;
+    let userPmt = bigUserPmt.map(function(each) {
+      return each.dataValues;
+    });
+    console.log("users Payment types?", userPmt);
     res.render('userAcctSettings', {
-      user
+      user,
+      userPmt
     })
   })
 };
