44ae36e45cdeb709b1fc53867e576af7034c1361
Joshua Meyers <joshua.r.meyers@gmail.com>
2017-10-25 10:58:48 -0500

    able to create order, then add product to order. or find existing order to add product to. needed to create prodorder join table manually to account for duplicate items on same order

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,6 @@ router.get('/', (req, res, next) => {
 // pipe all other requests through the route modules
 router.use(require('./authRoute'));
 router.use(require('./productRoute'));
+router.use(require('./orderRoute'));
 
 module.exports = router;
