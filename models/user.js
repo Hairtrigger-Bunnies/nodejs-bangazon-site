60b77ca43d97acdfddfce16736e3d9bf2663ef0c
Emily Lemmon <eclaire.lemmon@gmail.com>
2017-10-25 11:54:39 -0500

    update seeding rollDB to include user phone numbers and change their type to string

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ module.exports = function(sequelize, DataTypes) {
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     address: DataTypes.STRING,
-    phone: DataTypes.BIGINT 
+    phone: DataTypes.STRING 
   }, {timestamps: false});
 
   User.associate= (models) => {
